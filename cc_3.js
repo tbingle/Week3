b00d4748c5bede0eaf53b9f21b258eb1b42f3c6d
tbingle <sandmanthomas9@icloud.com>
2025-01-30 12:17:52 -0500

    Task 1 - Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -1,1 +1,8 @@
 //Inital Commit
+let prices = [7,3,8,5,6]; // Inital Prices
+console.log (prices)// Log
+prices.push (9);// New price added
+console.log (prices);// Log
+prices.shift();// Remove 1st price
+console.log (prices);// Log
+
