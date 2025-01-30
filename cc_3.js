d96196ef9509b262b118748970dcd5f10b300701
tbingle <sandmanthomas9@icloud.com>
2025-01-30 12:39:36 -0500

    Task 3- Employee Preformance Tracking.

--- cc_3.js
+++ cc_3.js
@@ -10,3 +10,14 @@ let orders = [1,2,3,4,5];// Order quantity
 console.log (orders);//Log
 orders [2] +=5;// Increase order by 5
 console.log (orders);//Log
+
+let employee = {
+    Name: "Aidan Mcdonell",
+    Role: "Technician",
+    PreformanceScore: 8,
+    isActive: true,
+    PromotonEligible: true // New Property Added
+} //Employee Information
+console.log (employee)//Log
+employee.PreformanceScore = 9
+console.log("Updated Preformance Score", employee);// Updated Preformace Score
